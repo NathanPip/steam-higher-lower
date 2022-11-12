320fe4b9778057df5e614cd3f1ee0d93abea36ab
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-11-11 17:59:54 -0800

    leaderboard data and start of styling

--- pages/leaderboards.tsx
+++ pages/leaderboards.tsx
@@ -14,6 +14,14 @@ export default function LeaderBoard({leaderBoard}: LeaderBoardProps) {
     <BackgroundLayout>
       <div className={`transition-opacity duration-1000 mx-auto flex flex-col items-center h-3/4`}>
         <h1 className="w-fit my-9 text-transparent text-8xl bg-clip-text bg-gradient-to-br from-blue-50 via-sky-600 to-black">LeaderBoards</h1>
+        <ul>
+            <li className="text-center">
+                <h2 className="text-4xl">Nather</h2>
+                scored
+                <h3 className="text-6xl">23</h3>
+                <p>which has been beaten 2 times matched 1 time and attempted 56 times</p>
+            </li>
+        </ul>
       </div>
     </BackgroundLayout>
   );
@@ -21,10 +29,15 @@ export default function LeaderBoard({leaderBoard}: LeaderBoardProps) {
 
 export const getServerSideProps: GetServerSideProps = async () => {
     try {
-      const scores = await prisma.highscore.findMany();
+      let scores = await prisma.highscore.findMany();
+      let scoreObjs = scores.map((scoreObj) => {
+        const {name, score, attempts, beaten, matched} = scoreObj;
+        return {name, score, attempts, beaten, matched};
+      })
+      scoreObjs = scoreObjs.sort((a,b) => a.score - b.score).reverse();
       return {
         props: {
-          leaderBoard: scores,
+          leaderBoard: scoreObjs,
           error: null,
         },
       };
